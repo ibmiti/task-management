c1fdc428d1e46a4c85caa9ec57b76f889f463270
Dimitri M <ibmiti@gmail.com>
2023-12-09 14:26:42 -0600

    tasks now sortable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,20 @@
 </head>
 <body>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.14.0/Sortable.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // 1. Select element after the document has loaded
+        // 2. initialize sortable instance on the task-list element which was selected in step 1.
+        // 3. handle event: on grap, allow dragging
+        // 4. apply animnation (occurs at 140 milliseconds)
+        document.addEventListener('DOMContentLoaded', function () {
+            const taskList = document.getElementById('task-list');
+            new Sortable(taskList, {
+                handle: '.card-header',
+                animation: 150
+            });
+        });
+    </script>
 </body>
 </html>
