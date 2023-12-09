8b53bc14712aaa803643da930b022667eda0ffea
Dimitri M <ibmiti@gmail.com>
2023-12-09 13:30:09 -0600

    added neccesary fields to table, via migration

--- database/migrations/2023_12_09_192518_create_tasks_table.php
+++ database/migrations/2023_12_09_192518_create_tasks_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('priority')->unsigned()->default(0);
             $table->timestamps();
         });
     }
