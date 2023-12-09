25a8ae79c807b123164b47895896ffa70cc8b018
Dimitri M <ibmiti@gmail.com>
2023-12-09 13:37:02 -0600

    added notes regarding most recent change

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,5 +12,6 @@ class Task extends Model
     // 1. now these fields are mass-assignable, when creating or updating tasks. via crud operations
     // 2. not all fields should be mass-assignable for security reasons, this protects against error or malicious actions
     // 3. mass-assignable = ability to update multiple fields during a single crud action (create,read, update,delete)
+    // 4. other fields, like id and timestamps will not be freely changeable, which protects integrity of the records
     protected $fillable = ['name', 'priority'];
 }
