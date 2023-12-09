1765e0a85c7bd575dfcd3d085c87e926998558b0
Dimitri M <ibmiti@gmail.com>
2023-12-09 13:35:15 -0600

    making neccessary fields, mass-assignable

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    // 1. now these fields are mass-assignable, when creating or updating tasks. via crud operations
+    // 2. not all fields should be mass-assignable for security reasons, this protects against error or malicious actions
+    // 3. mass-assignable = ability to update multiple fields during a single crud action (create,read, update,delete)
+    protected $fillable = ['name', 'priority'];
 }
