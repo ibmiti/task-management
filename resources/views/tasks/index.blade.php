f0d799546d921d22cc571cbc9ac078b12a1b977d
Dimitri M <ibmiti@gmail.com>
2023-12-09 14:19:28 -0600

    adding drag handle

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="container">
         <h2>Task List</h2>
-        <div class="row">
+        <div class="row" id="task-list">
             @foreach ($tasks as $task)
                 <div class="col-md-4 mb-4">
                     <div class="card">
+                        <div class="card-header" style="cursor: grab;">
+                            Drag Me
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">{{ $task->name }}</h5>
                             <p class="card-text">Priority: {{ $task->priority }}</p>
