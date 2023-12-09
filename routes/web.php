86803612ee5ba36916401af2154e362a6cb38a94
Dimitri M <ibmiti@gmail.com>
2023-12-09 13:51:13 -0600

    naming the routes

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index')->name('name');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create')->name('create_view');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store')->name('create_task');
 
 // edit
 
