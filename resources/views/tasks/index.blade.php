569617468d8afd9a0360826bd1ff25918c4f038e
Dimitri M <ibmiti@gmail.com>
2023-12-09 14:16:30 -0600

    added bootstrap and layout for cleaner views

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,8 +1,23 @@
-@foreach ($tasks as $task)
-    {{ $task->name }} - Priority: {{ $task->priority }}
-    <form action="{{ route('destroy_task', $task->id) }}" method="post">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Delete</button>
-    </form>
-@endforeach
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h2>Task List</h2>
+        <div class="row">
+            @foreach ($tasks as $task)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $task->name }}</h5>
+                            <p class="card-text">Priority: {{ $task->priority }}</p>
+                            <form action="{{ route('destroy_task', $task->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
